b2d0d414a2ee6cec0a283f81763d06fc01789ea4
ErickBeLike <20223l001053@utcv.edu.mx>
2024-03-22 01:37:26 -0600

    att MAGI

--- src/main/java/com/api/iot/repository/ValoresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.iot.repository;
-
-import com.api.iot.model.Valores;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValoresRepository extends MongoRepository <Valores, String> {
-}
